ff4f786f6bf3192527635c91110d1b0d21341aa1
Michael <hellomikko@outlook.com>
2023-03-05 03:16:43 -0500

    update package.json and svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	kit: {
-		adapter: adapter(),
-
-		// remove this if you're not using comment system
-		csp: { mode: 'auto' }
-	},
-	preprocess: [mdsvex(mdsvexConfig), vitePreprocess()]
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+  kit: {
+    adapter: autoAdapter(), // or netlifyAdapter()
+    csp: { mode: 'auto' } // remove this if you're not using comment system
+  },
+  preprocess: [mdsvex(mdsvexConfig), vitePreprocess()]
 };
 
 export default config;
