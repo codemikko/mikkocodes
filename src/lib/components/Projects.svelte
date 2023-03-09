5eda20ee25454eeec17ff44b86a64458d1168804
Michael <hellomikko@outlook.com>
2023-03-08 23:50:09 -0500

    update 5 files

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,15 +1,14 @@
 <script>
 	import Tag from '$lib/components/Tag.svelte';
 	import Title from '$lib/components/Title.svelte';
-	import SearchBox from '$lib/components/SearchBox.svelte';
 	import { page } from '$app/stores';
 	import fuzzySearch from '$utils/search.js';
 
 	export let title = '';
 	export let link_type = '';
-	export let demo = '';
-	export let blog ='';
-	export let github ='';
+	// export let demo = '';
+	// export let blog = '';
+	// export let github = '';
 	export let subtitle = '';
 	export let projects = [];
 	export let tags = [];
@@ -32,13 +31,9 @@
 				<Title {title} {subtitle} {h2} />
 			</div>
 
-			<div class="pl-4" class:border-l-2={search}>
-				{#if search}
-					<SearchBox />
-				{/if}
-
+			<div class="pl-4">
 				{#if tags.length}
-					<div class="flex flex-wrap">
+					<div class="flex flex-wrap class:border-l-2={search}">
 						{#each tags as tag}
 							<div class="mr-5">
 								<Tag text={tag.text} size="text-xs" />
@@ -67,13 +62,10 @@
 								class="text-sm font-medium leading-5 text-gray-500 xl:text-right xl:col-span-1"
 							/>
 							<div class="space-y-5 xl:col-span-3">
-								<div class="space-y-6">
+								<div class="space-y-9">
 									<div>
 										<h2 class="text-2xl font-bold leading-8 tracking-tight">
-											<a
-												href={`/blog/${project.slug}`}
-												class="text-gray-900 dark:text-gray-100"
-											>
+											<a href={`/blog/${project.slug}`} class="text-gray-900 dark:text-gray-100">
 												{project.title}
 											</a>
 										</h2>
@@ -92,27 +84,43 @@
 											{/each}
 										</div>
 									</div>
+									{#if project.image}
+										<div class=" w-5/6 pb-1">
+											<img
+												alt={project.title}
+												src={project.image}
+												class="w-3/5 h-auto drop-shadow-xl"
+											/>
+										</div>
+									{/if}
 									<div class="prose max-w-none text-gray-500 dark:text-gray-400">
 										{project.description}
 									</div>
 
-
-									{#if link_type === 'demo'}
-											<a href={demo} class="button text-sm bg-orange-500 shadow-lg shadow-orange-500/50 pb-1 pt-1 pr-4 pl-4 accent-current/0 rounded-lg">
-												<i class="fa-solid fa-link" /> Demo
-											</a>
-											{:else if link_type === 'github'}
-											<a href={github} class="button text-sm bg-blue-500 shadow-lg shadow-blue-500/50 pb-1 pt-1 pr-4 pl-4 accent-current/0 rounded-lg">
-												<i class="fa-brands fa-github" /> GitHub
-											</a>
-											{:else if link_type === 'blog'}
-											<a href={blog} class="button text-sm bg-purple-500 shadow-lg shadow-purple-500/50 pb-1 pt-1 pr-4 pl-4 accent-current/0 rounded-lg">
-												<i class="fa-solid fa-link" /> Blog
-											</a>
+									{#if 'demo' in link_type && project.demo}
+										<a
+											href={project.demo}
+											class="button text-sm bg-orange-500 shadow-lg shadow-orange-500/50 pb-1 pt-1 pr-4 pl-4 accent-current/0 rounded-lg"
+										>
+											<i class="fa-solid fa-link" /> Demo
+										</a>
 									{/if}
-
-
-
+									{#if 'github' in link_type && project.github}
+										<a
+											href={project.github}
+											class="button text-sm bg-blue-500 shadow-lg shadow-blue-500/50 pb-1 pt-1 pr-4 pl-4 accent-current/0 rounded-lg"
+										>
+											<i class="fa-brands fa-github" /> GitHub
+										</a>
+									{/if}
+									{#if 'blog' in link_type && project.blog}
+										<a
+											href={project.blog}
+											class="button text-sm bg-purple-500 shadow-lg shadow-purple-500/50 pb-1 pt-1 pr-4 pl-4 accent-current/0 rounded-lg"
+										>
+											<i class="fa-solid fa-link" /> Blog
+										</a>
+									{/if}
 								</div>
 							</div>
 						</div>
--- src/lib/components/SearchBox.svelte
+++ src/lib/components/SearchBox.svelte
@@ -28,7 +28,7 @@
 			name="search"
 			aria-label="Search posts"
 			type="text"
-			placeholder="Search posts"
+			placeholder="Search Posts or Projects"
 			class="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
 		/>
 		<svg
