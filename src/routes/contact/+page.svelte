0a767f34d8d4f34023da6097bae440b3da994c7a
Michael <hellomikko@outlook.com>
2023-03-12 03:33:56 -0400

    :sparkles: Totally awesome Social Grid

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -22,90 +22,132 @@
 	<!-- <div class="flex flex-col items-center py-6 ">
 			<i class="fa-solid fa-file-contract" style="width: 44px; height: 92px;"></i>
 		</div> -->
-			<header class="space-y-2 my-12">
-			  <p>
-				If you have any questions, feel free to contact me.
-			  </p>
-			</header>
-			<!-- Template Editing Starts Here -->
+	<header class="space-y-2 my-12">
+		<p>If you have any questions, feel free to contact me.</p>
+	</header>
+	<!-- Template Editing Starts Here -->
 
+	<div class="grid grid-cols-2 gap-0">
 
-			<div class="grid grid-cols-2 gap-4">
-		  <div class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800 hover:bg-neutral-800">
+		<a href="https://reflinked.mikko.codes/discordserv" target="_blank" rel="noreferrer">
+		<div
+			class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800 hover:bg-neutral-800"
+		>
 			<div class="overflow-x-hidden leading-relaxed space-y-2">
-			  <h2 class="font-medium text-black dark:text-white truncate">Discord</h2>
-			  <p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
-				<span class="text-black/50 dark:text-white/30">Follow my Discord server</span>
-			  </p>
+				<h2 class="font-medium text-black dark:text-white truncate">Discord</h2>
+				<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
+					<span class="text-black/50 dark:text-white/30">Follow my Discord server</span>
+				</p>
 			</div>
-			<span class="fab fa-discord text-2xl"></span>
-		  </div>
-		  
-		  
-			  <div class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800">
-				<div class="overflow-x-hidden leading-relaxed space-y-2">
-				  <h2 class="font-medium text-black dark:text-white truncate">Twitter</h2>
-				  <p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
+			<span class="fab fa-discord text-2xl" />
+		</div></a>
+
+		<a href="https://reflinked.mikko.codes/twitter" target="_blank" rel="noreferrer">
+		<div
+			class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800"
+		>
+			<div class="overflow-x-hidden leading-relaxed space-y-2">
+				<h2 class="font-medium text-black dark:text-white truncate">Twitter</h2>
+				<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
 					<span class="text-black/50 dark:text-white/30">Follow me on Twitter</span>
-				  </p>
-				</div>
-				<span class="fab fa-twitter text-2xl"></span>
-			  </div>
-		  
-			  <div class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800">
-				<div class="overflow-x-hidden leading-relaxed space-y-2">
-				  <h2 class="font-medium text-black dark:text-white truncate">Github</h2>
-				  <p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
-					<span class="text-black/50 dark:text-white/30">Follow me on Github</span>
-				  </p>
-				</div>
-				<span class="fab fa-github text-2xl"></span>
-			  </div>
-		  
-			  <div class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800">
-				<div class="overflow-x-hidden leading-relaxed space-y-2">
-				  <h2 class="font-medium text-black dark:text-white truncate">LinkedIn</h2>
-				  <p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
+				</p>
+			</div>
+			<span class="fab fa-twitter text-2xl" />
+		</div>
+</a>
+
+		<a href="https://reflinked.mikko.codes/github" target="_blank" rel="noreferrer">
+		<div
+			class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800"
+		>
+			<div class="overflow-x-hidden leading-relaxed space-y-2">
+				<h2 class="font-medium text-black dark:text-white truncate">CodePen</h2>
+				<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
+					<span class="text-black/50 dark:text-white/30">See my other pens I've made and used</span>
+				</p>
+			</div>
+			<span class="fa-brands fa-codepen text-2xl" />
+		</div>
+	</a>
+
+		<a href="https://reflinked.mikko.codes/linkedin" target="_blank" rel="noreferrer">
+		<div
+			class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800"
+		>
+			<div class="overflow-x-hidden leading-relaxed space-y-2">
+				<h2 class="font-medium text-black dark:text-white truncate">LinkedIn</h2>
+				<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
 					<span class="text-black/50 dark:text-white/30">Connect with me on LinkedIn</span>
-				  </p>
-				</div>
-				<span class="fab fa-linkedin-in text-2xl"></span>
-			  </div>
-		  <div class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800">
-			  <div class="overflow-x-hidden leading-relaxed space-y-2">
+				</p>
+			</div>
+			<span class="fab fa-linkedin-in text-2xl" />
+		</div>
+		</a>
+
+		<a href="https://reflinked.mikko.codes/fbmikkocodes" target="_blank" rel="noreferrer">
+		<div
+			class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800"
+		>
+			<div class="overflow-x-hidden leading-relaxed space-y-2">
 				<h2 class="font-medium text-black dark:text-white truncate">Facebook Messenger</h2>
 				<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
-				  <span class="text-black/50 dark:text-white/30">Chat with me on Facebook Messenger</span>
+					<span class="text-black/50 dark:text-white/30">Chat with me on Facebook Messenger</span>
 				</p>
-			  </div>
-			  <span class="fab fa-facebook-messenger text-2xl"></span>
 			</div>
-		  
-			<div class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800">
-			  <div class="overflow-x-hidden leading-relaxed space-y-2">
-				<h2 class="font-medium text-black dark:text-white truncate">Email</h2>
-				<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
-				  <span class="text-black/50 dark:text-white/30">Send me an email</span>
-				</p>
-			  </div>
-			  <span class="fas fa-envelope text-2xl"></span>
+			<span class="fab fa-facebook-messenger text-2xl" />
+		</div>
+		</a>
+
+		<a href="https://reflinked.mikko.codes/proton-safeOn" target="_blank" rel="noreferrer">
+			<div
+				class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800"
+			>
+				<div class="overflow-x-hidden leading-relaxed space-y-2">
+					<h2 class="font-medium text-black dark:text-white truncate">Email</h2>
+					<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
+						<span class="text-black/50 dark:text-white/30">Send me an email</span>
+					</p>
+				</div>
+				<span class="fas fa-envelope text-2xl" />
 			</div>
-		  </div>
-		  
-		  <div class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800">
-			<div class="overflow-x-hidden leading-relaxed space-y-2">
-			  <h2 class="font-medium text-black dark:text-white truncate">Codepen</h2>
-			  <p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
-				<span class="text-black/50 dark:text-white/30">Check out my Pens! I do alot of theory testing there sometimes before bringing it to my VSCode.</span>
-			  </p>
+		</a>
+
+		<a href="https://reflinked.mikko.codes/github" target="_blank" rel="noreferrer">
+			<div
+				class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800"
+			>
+				<div class="overflow-x-hidden leading-relaxed space-y-2">
+					<h2 class="font-medium text-black dark:text-white truncate">Github</h2>
+					<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
+						<span class="text-black/50 dark:text-white/30">Check out my other repositories</span>
+					</p>
+				</div>
+				<i class="fab fa-github text-2xl" />
 			</div>
-			<span class="fa-brands fa-codepen"></span>
-		  </div>
+		</a>
+
+
+		<a href="https://reflinked.mikko.codes/JjapjZp" target="_blank" rel="noreferrer">
+			<div
+				class="rounded-lg card-base rounded-md overflow-x-hidden transition-colors p-4 cursor-pointer items-center flex space-x-4 justify-between hover:bg-neutral-800"
+			>
+				<div class="overflow-x-hidden leading-relaxed space-y-2">
+					<h2 class="font-medium text-black dark:text-white truncate">Get This Pen</h2>
+					<p class="text-black/50 dark:text-white/30 text-sm line-clamp-2">
+						<span class="text-black/50 dark:text-white/30">Do you like this pen? Grab it here</span>
+					</p>
+				</div>
+				<i class="fa-solid fa-code-fork text-2xl" />
+			</div>
+		</a>
+	</div>
+	
 </div>
-				
 
 
-				
+
+
+
 
 <!-- svelte-ignore a11y-missing-attribute -->
 <iframe
