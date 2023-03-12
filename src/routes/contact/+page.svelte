ae7e8f41923aed9250ce5ea2c6554b57551e5d96
Michael <hellomikko@outlook.com>
2023-03-11 21:19:39 -0500

    Created as a Template for other future pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	import Head from '$lib/components/layout/Head.svelte';
 	import SocialIcon from '$lib/components/SocialIcon.svelte';
-	import { config } from '$lib/config';
-	import { user } from '$lib/config';
+	// import { config } from '$lib/config';
+	// import { user } from '$lib/config';
 	import Title from '$lib/components/Title.svelte';
 
-	export let data;
-	const authors = data.authors;
+	// export let data;
+	// const authors = data.authors;
 </script>
 
 <Head title="Contact" />
@@ -19,8 +19,9 @@
 		class="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0 grid grid-cols-1 gap-12 sm:grid-cols-3 sm:items-center shadow-lg pb-14"
 	>
 		<div class="flex flex-col items-center py-6 ">
-			<img src={user.siteAvatar} alt="logo" class="w-36" /><!--  Add a Contact type of logo -->
+			<i class="fa-solid fa-file-contract" style="width: 44px; height: 92px;"></i>
 		</div>
+		
 		<div class="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
 			Add something for them to contact me with
 </div>
